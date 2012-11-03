d52c42c48cdfb42fd22d9f5d9752e55a4f122f68
Jason Lombardozzi <jason.lombardozzi@gmail.com>
2012-11-03 17:04:23 -0400

    Create base app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrisisHelper::Application.config.secret_token = '824d3e58709ff98e1275b217b2c9840219d5bf1ba9c119eac5a99169b240f1a10e281a4bca3de6e491aa5c54b2675ebf2b75ff836617a23a93e77ca234c20ae4'
