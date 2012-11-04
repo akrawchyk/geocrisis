d3459d9ccaee593a672aa42148c14d44759733f4
Px3 <thepechinator.com>
2012-11-04 04:36:11 -0500

    some minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ CrisisHelper::Application.routes.draw do
   root to: 'static_pages#landing_page'
   match '/home' => 'static_pages#home'
   match '/contacts' => 'static_pages#contact_list'
+  match '/tweets' => 'static_pages#item_list'
 
   get 'locations/:id' => 'locations#show', as: :location
   get 'locations' => 'locations#show', constraints: LocationByGeoipConstraint, as: :location_by_geoip
