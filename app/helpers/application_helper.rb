9e6469e87bfe50d03bd290bd19633b35914759f0
Andrew Krawchyk <akrawchyk@gditweb.com>
2012-11-04 16:34:50 -0500

    finalize tweets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  require 'open-uri'
+  def googGeoIP(query)
+    return JSON.parse(open("#{Rails.application.config.goog_geocode_url}&address=#{query}&rankby=distance&sensor=false").read)['results']
+  end
 end
