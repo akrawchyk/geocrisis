3134459cf3b6199e38d3a518cc3ab09b58ad043d
Px3 <thepechinator.com>
2012-11-04 01:25:21 -0500

    adding back button to local tweets page

--- app/controllers/local_tweets_controller.rb
+++ app/controllers/local_tweets_controller.rb
@@ -1,5 +1,7 @@
 class LocalTweetsController < ApplicationController
   def show
     @tweets = Twitter.search("#sandy", :geocode => '38.865805,-77.137318,2mi', :lang => 'en', :count => 30, :result_type => 'recent').results
+  	@nav_extra_klass = "downstream"
+  	@show_back_button = true  
   end
 end
