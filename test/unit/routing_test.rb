7a1ad8fe7c93ef84d78167c4d7c364b1814f6845
Jason Lombardozzi <jason.lombardozzi@gmail.com>
2012-11-03 23:43:10 -0400

    Location controller addition, delete fixtures, routing changes

--- test/unit/routing_test.rb
+++ test/unit/routing_test.rb
@@ -1,10 +1,22 @@
 require 'test_helper'
-class LocationTest < ActiveSupport::TestCase
-  test "location routes" do
+class RoutingTest < ActiveSupport::TestCase
+  test "location by id" do
     assert_generates "/location/1", {controller: "location", action: "show", id: "1"}
-    #assert_generates "/location?latlng=-39.76144296429947,-104.8011589050293", {controller: "location", action: "show", latlng: "39.76144296429947,-104.8011589050293"}
-    #assert_generates "/location?latlng=39.76144296429947,-104.8011589050293", {controller: "location", action: "show", latlng: "39.76144296429947,-104.8011589050293"}
-    #assert_generates "/location?latlng=-39.76144296429947,104.8011589050293", {controller: "location", action: "show", latlng: "39.76144296429947,-104.8011589050293"}
-    #assert_generates "/location?zipcode=21771", {controller: "location", action: "show", zipcode: "21771"}
+  end
+
+  test 'location by geoip two negatives' do
+    assert_generates "/location?latlng=-39.76144296429947,-104.8011589050293", {controller: "location", action: "show", latlng: "39.76144296429947,-104.8011589050293"}
+  end
+
+  test 'location by geoip one negative' do
+    assert_generates "/location?latlng=39.76144296429947,-104.8011589050293", {controller: "location", action: "show", latlng: "39.76144296429947,-104.8011589050293"}
+  end
+
+  test 'location by geoip no negatives' do
+    assert_generates "/location?latlng=-39.76144296429947,104.8011589050293", {controller: "location", action: "show", latlng: "39.76144296429947,-104.8011589050293"}
+  end
+
+  test 'location by zipcode' do
+    assert_generates "/location?zipcode=21771", {controller: "location", action: "show", zipcode: "21771"}
   end
 end
