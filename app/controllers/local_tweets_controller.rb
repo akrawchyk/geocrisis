e43412e1a3b4a42294cdbea25c3fe5c6f6035bda
Andrew Krawchyk <akrawchyk@gditweb.com>
2012-11-04 11:46:35 -0500

    change how nav bar menus hide buttons

--- app/controllers/local_tweets_controller.rb
+++ app/controllers/local_tweets_controller.rb
@@ -1,7 +1,6 @@
 class LocalTweetsController < ApplicationController
   def show
+    # TODO
     @tweets = Twitter.search("#sandy", :geocode => '38.865805,-77.137318,2mi', :lang => 'en', :count => 30, :result_type => 'recent').results
-    @nav_extra_klass = "downstream"
-    @show_back_button = true
   end
 end
